63aa32a0e5196fa6e8f5432617ca1b77e5d8002f
mrradd <conrad.horton@gmail.com>
2019-05-02 11:39:17 -0700

    More work on data grid.

--- frontend/src/InitiativeTracker5e.js
+++ frontend/src/InitiativeTracker5e.js
@@ -18,26 +18,7 @@ export class InitiativeTracker5e extends React.Component{
         <h1>Test</h1>
         <Clock/>
         <TurnIndicator/>
-        <ActionButtons/>
         <DataGrid/>
-      </div>
-    );
-  }
-}
-
-/******************************************************************************
-* Class ActionButtons *
-* Buttons that allow some interaction with the datagrid.
-******************************************************************************/
-class ActionButtons extends React.Component{
-  constructor(props){
-    super(props);
-  }
-  
-  render(){
-    return (
-      <div>
-        <h3>This is where some buttons go.</h3>
       </div>
     );
   }
@@ -61,6 +42,12 @@ class Clock extends React.Component{
   }
 }
 
+const turnOwnerRow = () => {
+  return(
+    
+  );
+}
+
 /******************************************************************************
 * Class DataGrid *
 * Displays a list of the characters and some info about them.
@@ -69,39 +56,103 @@ class DataGrid extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      rows: [{id: 0, name: 'derp'}],
-      columns: [{key:'id', name: 'ID'}, {key:'name', name:'Name'}],
+      rows: [],
+      columns: [
+        {key:'name',       name:'Name',       editable: true},
+        {key:'initiative', name:'Initiative', editable: true},
+        {key:'hp',         name:'HP',         editable: true},
+        {key:'ac',         name:'AC',         editable: true},
+        {key:'spellDC',    name:'Spell DC',   editable: true},
+        {key:'notes',      name:'Notes',      editable: true}
+      ],
       idCounter: 0
     };
   }
   
-  createRows = () => {
-    var rows = [{id: this.state.idCounter, name: 'derp'}];
-    
-    this.state.setState({rows: rows});
-    
-    return rows;
-  };
-  
+  /**
+   * Adds a row to the row list.
+   */
   addRow = () => {
-    var idCounter = this.state.idCounter++;
+    var idCounter = this.state.idCounter + 1;
     var rows = this.state.rows.slice();
     rows.push({id:idCounter, name: "derp " + idCounter});
     
-    this.state.setState({idCounter: idCounter, rows: rows});
+    this.setState({idCounter: idCounter, rows: rows, turnOwner: false});
   };
   
-  createColumns = () => {
-    return [{key:'id', name: 'ID'}, {key:'name', name:'Name'}];
+  /**
+   * Updates the turn index to the next character.
+   */
+  nextTurn = () => {
+    var rows           = this.state.rows;
+    var turnOwnerIndex = -1;
+
+    /** Find the turn owner. */
+    for(var i = 0; i < rows.length - 1; i++){
+
+      /** Update turn owner to next character. */
+      if(rows[i].turnOwner){
+        
+        /** Clear out current turn owner. */
+        turnOwnerIndex = i;
+        rows[turnOwnerIndex].turnOwner = false;
+        
+        /** Go to index 0 since we are going out of bounds. */
+        if(turnOwnerIndex + 1 > rows.length - 1){
+          rows[0].turnOwner = true;
+        }
+        
+        /** Set the index to next character. */
+        else{
+          rows[turnOwnerIndex + 1].turnOwner = true;
+        }
+
+        break;
+      }
+    }
+
+    /** No turn owner was found, so set it the the 0 index. */
+    if(turnOwnerIndex < 0){
+      rows[0].turnOwner = true;
+    }
+
+    this.setState({rows: rows});
+  }
+
+  /**
+   * Allows the grid to be updated.
+   */
+  onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+    this.setState(state => {
+      const rows = state.rows.slice();
+      for (let i = fromRow; i <= toRow; i++) {
+        rows[i] = { ...rows[i], ...updated };
+      }
+      return { rows };
+    });
   };
-  
+
+  /**
+   * Updates the turn index to the previous character.
+   */
+  prevTurn = () => {
+
+  }
+
   render(){
     return (
-      <ReactDataGrid
-        columns={this.state.columns}
-        rowGetter={i => this.state.rows[i]}
-        rowsCount={500}
-      />
+      <div>
+        <button onClick={this.addRow}>Add Character</button>
+        <button onClick={this.nextTurn}>Next Turn</button>
+        <button onClick={this.prevTurn}>Previous Turn</button>
+        <ReactDataGrid
+          columns={this.state.columns}
+          rowGetter={i => this.state.rows[i]}
+          rowsCount={500}
+          onGridRowsUpdated={this.onGridRowsUpdated}
+          enableCellSelect={true}
+        />
+      </div>
     );
   }
 }
